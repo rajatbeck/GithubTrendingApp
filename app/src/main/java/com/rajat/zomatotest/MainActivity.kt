067a7840f5d8010ea98063f4b13cc61eb14e7d37
rajatbeck <rajatmarcelb@gmail.com>
2020-04-04 15:42:52 +0530

    my first commit

---
+++ app/src/main/java/com/rajat/zomatotest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rajat.zomatotest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rajat.zomatotest.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
