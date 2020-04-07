94634b21cd68b77e432c62f5e510392196e16d0d
rajatbeck <rajatmarcelb@gmail.com>
2020-04-07 12:31:28 +0530

    render the Design in UI

--- app/src/main/java/com/rajat/zomatotest/ui/main/MainFragment.kt
+++ app/src/main/java/com/rajat/zomatotest/ui/main/MainFragment.kt
@@ -10,10 +10,15 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rajat.zomatotest.R
 import com.rajat.zomatotest.ZomatoApp
+import com.rajat.zomatotest.models.Repository
+import com.rajat.zomatotest.models.Resource
 import com.rajat.zomatotest.utils.ViewModelFactory
 import kotlinx.android.synthetic.main.layout_error_view.*
+import kotlinx.android.synthetic.main.main_fragment.*
 import javax.inject.Inject
 
 class MainFragment : Fragment() {
@@ -27,6 +32,8 @@ class MainFragment : Fragment() {
     lateinit var factory:ViewModelProvider.Factory
 
     private val viewModel:MainViewModel by lazy { ViewModelProvider(requireActivity(),factory).get(MainViewModel::class.java) }
+
+    private val mainAdapter:MainAdapter by lazy { MainAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         inject()
@@ -48,11 +55,39 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.observeTrendingRepository().observe(viewLifecycleOwner, Observer {
-            Log.d(MainFragment.TAG,it.toString())
-        })
+        initView()
+        viewModel.observeTrendingRepository().observe(viewLifecycleOwner, Observer { renderUI(it) })
         viewModel.fetchTrendingGitHubRepository(forceFetch = false)
         btnRetry.setOnClickListener { viewModel.fetchTrendingGitHubRepository(forceFetch = false) }
+    }
+
+    private fun initView(){
+        with(rvRepositoryList){
+             layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+             addItemDecoration(DividerItemDecoration(requireContext(),LinearLayoutManager.VERTICAL))
+             adapter = mainAdapter
+        }
+    }
+
+    private fun renderUI(uiResponse:Resource<List<Repository>>){
+        when(uiResponse){
+            is Resource.Success ->{
+                shimmerViewContainer.visibility = View.GONE
+                errorView.visibility = View.GONE
+                rvRepositoryList.visibility = View.VISIBLE
+                mainAdapter.submitList(uiResponse.data)
+            }
+            is Resource.Error -> {
+                shimmerViewContainer.visibility = View.GONE
+                errorView.visibility = View.VISIBLE
+                rvRepositoryList.visibility = View.GONE
+            }
+            is Resource.Loading -> {
+                shimmerViewContainer.visibility = View.VISIBLE
+                errorView.visibility = View.GONE
+                rvRepositoryList.visibility = View.GONE
+            }
+        }
     }
 
 
